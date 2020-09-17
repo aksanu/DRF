de1b52bc4d7ba8b9ad55f61258e920ee21549eda
aksanu <ankit21april@gmail.com>
2020-09-17 14:42:03 +0530

    asdasd

--- customerapi/settings.py
+++ customerapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!f8fb)_8*qlqo=z0!$e+!ye7g(p%!x57xlewe23b(#r61b+emn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
