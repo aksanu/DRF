f320806e544c3221db63aa75c3830109ced8cd80
aksanu <ankit21april@gmail.com>
2020-09-21 17:00:52 +0530

    added db

--- customerapi/settings.py
+++ customerapi/settings.py
@@ -100,6 +100,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=600)
+
+DATABASES['default'].update(db_from_env)
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -109,6 +114,7 @@ DATABASES = {
 #         'HOST': 'localhost',
 #     }
 # }
+
 
 
 
